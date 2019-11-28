bf441abe71dd0e81ff37d42cd04749116401a7d8
Jess Layton <jessylayton@yahoo.co.uk>
2019-11-28 11:22:49 +0000

    First commit

---
+++ src/arrays/Arrays.java
@@ -0,0 +1,36 @@
+package arrays;
+
+public class Arrays {
+	public static void main(String[] args) {
+		int [] array1 = new int[10]; 
+		
+		for (int i = 0; i < array1.length; i++) {
+			array1[i] = 1;
+		}
+		
+		
+		
+		for (int i = 0; i < array1.length; i++) 
+			System.out.print(" " + array1[i]);
+		
+		System.out.println();
+		
+		int [] array2 = new int[10]; 
+		
+		for (int i = 0; i < array2.length; i++) {
+			array1[i] = 2;
+		}
+		
+		
+		
+		for (int i = 0; i < array1.length; i++) 
+			System.out.print(" " + array1[i]);
+		
+		
+			  
+		}
+	
+
+
+}
+
